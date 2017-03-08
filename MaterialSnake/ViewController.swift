6389f9ec1205f18da012f5ee8f04398d33680b8e
Alexandru Stoica <stoica.alexandru2000@gmail.com>
2017-03-08 06:57:04 +0200

    Multiple Direction Support

--- MaterialSnake/ViewController.swift
+++ MaterialSnake/ViewController.swift
@@ -1,25 +1,16 @@
-//
-//  ViewController.swift
-//  MaterialSnake
-//
-//  Created by Alexandru Stoica on 08/03/2017.
-//  Copyright © 2017 Alexandru Stoica. All rights reserved.
-//
 
 import UIKit
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
 
