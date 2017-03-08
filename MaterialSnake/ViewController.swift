7042d33afd894d41e11f46829f34fb306d0ab0b0
Alexandru Stoica <stoica.alexandru2000@gmail.com>
2017-03-08 05:49:24 +0200

    Initial Commit

---
+++ MaterialSnake/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MaterialSnake
+//
+//  Created by Alexandru Stoica on 08/03/2017.
+//  Copyright © 2017 Alexandru Stoica. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
