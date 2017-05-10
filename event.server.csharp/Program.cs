d73bb4ebd7e1444f85e44b24975460952f674f59
Alexandru <Alexandru@DESKTOP-NENSE3P>
2017-05-10 12:45:13 +0300

    Update

---
+++ event.server.csharp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using EventManager.Service;
+using Thrift.Server;
+using Thrift.Transport;
+
+namespace @event.server.csharp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                var eventServer = new ConnectionManager();
+                var processor = new Subsciption.Processor(eventServer);
+                TServerTransport transport = new TServerSocket(9091);
+                TServer server = new TThreadedServer(processor, transport);
+                Console.WriteLine("Starting the server ...");
+                server.Serve();
+                Console.Read();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+                Console.Read();
+            }
+            Console.WriteLine("Done");
+        }
+    }
+}
